28e7b6d0f055399ff0dae9f8797817afb037d4bc
CodingBel <84908127+CodingBel@users.noreply.github.com>
2024-05-23 14:48:40 +0200

    first commit

--- src/main.c
+++ src/main.c
@@ -1,43 +1,35 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   400
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define LED		DT_NODELABEL(led4)
+#define BUTTON 	DT_NODELABEL(button0)
+
+static const struct gpio_dt_spec led_spec = GPIO_DT_SPEC_GET(LED, gpios);
+static const struct gpio_dt_spec button_spec = GPIO_DT_SPEC_GET(BUTTON, gpios); 
+
+static struct gpio_callback button_cb; 
+
+void button_pressed_callback (const struct device *gpiob, struct gpio_callback *cb, gpio_port_pins_t pins){
+	gpio_pin_toggle_dt (&led_spec);
+}
 
 int main(void)
 {
-	int ret;
+	gpio_pin_configure_dt(&led_spec, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&button_spec, GPIO_INPUT);
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+	gpio_init_callback(&button_cb, button_pressed_callback, BIT(button_spec.pin));
+	gpio_add_callback(button_spec.port, &button_cb);
+	gpio_pin_interrupt_configure_dt(&button_spec, GPIO_INT_EDGE_TO_ACTIVE);
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+	while(1){
 		k_msleep(SLEEP_TIME_MS);
 	}
-	return 0;
+	return 0; 
 }
